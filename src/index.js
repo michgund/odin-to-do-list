20df500d425ae766db679a73848603abeb06a219
michgund <117133781+michgund@users.noreply.github.com>
2023-06-27 15:40:38 +0900

    add new project modal

--- src/index.js
+++ src/index.js
@@ -31,6 +31,29 @@ function tab() {
   return element;
 }
 
+function createModal() {
+  const dialog = document.createElement("dialog");
+  dialog.setAttribute("id", "favDialog");
+  dialog.innerHTML =
+    '<form method="dialog"> <p> <label for="project">Project Name:</label><input type="text" name="project" id="project"></p><p> <label for="description">Project Description:</label><textarea name="description" id="description"></textarea></p><p> <label for="priority">Priority</label> <select id="priority" name="priority"> <option>Choose</option> <option>Low</option> <option>Medium</option> <option>High</option> </select> </p> <div> <button id="cancel" type="reset">Cancel</button> <button type="submit">Confirm</button> </div> </form>';
+  return dialog;
+}
+
 document.body.appendChild(banner());
 document.body.appendChild(tabs());
+document.body.appendChild(createModal());
 document.querySelector(".tabs").appendChild(tab());
+
+document
+  .querySelector(".new-project")
+  .addEventListener("click", handleNewProjectClick);
+
+function handleNewProjectClick() {
+  console.log("loggin");
+  const dialog = document.querySelector("#favDialog");
+  dialog.showModal();
+
+  document.querySelector("#cancel").addEventListener("click", () => {
+    dialog.close();
+  });
+}
