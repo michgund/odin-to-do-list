44eb0df71601c6d7819a9025e0d83bbce90c6051
michgund <117133781+michgund@users.noreply.github.com>
2023-06-27 14:18:31 +0900

    add basic styling

--- src/index.js
+++ src/index.js
@@ -2,17 +2,35 @@ import "./style.css";
 import printMe from "./print";
 // import Icon from "./icon.png";
 
-function component() {
+function banner() {
   const element = document.createElement("div");
-  element.textContent = "Todo";
-
-  // Add the image to our existing div.
-  //   const myIcon = new Image();
-  //   myIcon.src = Icon;
-
-  //   element.appendChild(myIcon);
+  element.classList.add("banner");
+  const title = document.createElement("h1");
+  title.textContent = "Todo";
+  element.appendChild(title);
 
   return element;
 }
 
-document.body.appendChild(component());
+function tabs() {
+  const element = document.createElement("div");
+  element.classList.add("tabs");
+
+  return element;
+}
+
+function tab() {
+  const element = document.createElement("div");
+  element.classList.add("tab");
+  element.textContent = "New Project";
+  const plus = document.createElement("button");
+  plus.classList.add("new-project");
+  plus.innerHTML = "+";
+  element.appendChild(plus);
+
+  return element;
+}
+
+document.body.appendChild(banner());
+document.body.appendChild(tabs());
+document.querySelector(".tabs").appendChild(tab());
